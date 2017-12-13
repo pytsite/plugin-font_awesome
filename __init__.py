482ea5a09c9372513784c1dcf07113b84173a240
Alexander Shepetko <a@shepetko.com>
2017-12-13 17:13:13 +0200

    Release 1.1

--- __init__.py
+++ __init__.py
@@ -6,16 +6,13 @@ __email__ = 'a@shepetko.com'
 __license__ = 'MIT'
 
 
-def _init():
+def plugin_load():
     from plugins import assetman
 
     assetman.register_package(__name__)
-    assetman.t_css(__name__ + '@**')
-    assetman.t_copy_static(__name__ + '@**')
+    assetman.t_css(__name__)
+    assetman.t_copy_static(__name__)
     assetman.js_module('font-awesome', __name__ + '@font-awesome')
     assetman.library('font-awesome', [
         'font_awesome@css/font-awesome.css'
     ])
-
-
-_init()
